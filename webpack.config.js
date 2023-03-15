ddc8c2feca29d037c2a69334a037a6325e277019
sau <dontsendsamspam@gmail.com>
2023-03-15 16:37:39 -0700

    Set up the project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  devServer: {
+    static: './dist',
+  },
+  entry: {
+    main: './src/program/to-do.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'to-do-bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index-template.html',
+      title: 'To Do App',
+      filename: 'index.html'
+    }),
+    new webpack.SourceMapDevToolPlugin({}),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
