465ca698f4899587ec17bce73be942b850d99410
sau <dontsendsamspam@gmail.com>
2023-03-21 21:48:27 -0700

    Create demo projects and an array to store them

--- src/program/to-do.js
+++ src/program/to-do.js
@@ -1,1 +1,31 @@
 import "../style/default.css";
+
+const emptyProject = {
+  title: "",
+  description: "",
+  tasks: [],
+};
+
+const demoProject1 = {
+  title: "Exercise solutions.",
+  description:
+    "Figure out an optimal exercise setup for each major muscle group.",
+  tasks: [],
+};
+
+const demoProject2 = {
+  title: "250 box challenge",
+  description: '"draw a box - then do it again another 249 times."',
+  tasks: [],
+};
+
+const projects = [
+  { project: emptyProject },
+  { project: demoProject1 },
+  { project: demoProject2 },
+];
+
+console.log(projects);
+console.log(projects[0]);
+console.log(projects[1]);
+console.log(projects[2]);
