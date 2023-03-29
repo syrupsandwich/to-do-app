10b392cbc9805fe6087e4bb13ff2ffc5d28ca596
sau <dontsendsamspam@gmail.com>
2023-03-29 12:19:54 -0700

    Add category factory and relevant info to print
    Wrap the app logic in a factory that will make category objects which
    will contain projects. The purpose is to organize projects.

--- src/program/main.js
+++ src/program/main.js
@@ -1,27 +1,72 @@
 import "../style/default.css";
-import {
-  projects,
-  printProjects,
-  moveProject,
-  removeProject,
-  makeProject,
-  printTasks,
-  addTask,
-  updateProject,
-  updateTask,
-  removeTask,
-  transferTask,
-  moveTask,
-} from "./to-do.js";
+import { makeCategory } from "./to-do.js";
 
-printProjects();
-makeProject({
-  title: "to-to yapp",
-  description: "i wish i new how to test features automatically...",
+const demo1 = makeCategory("Demo");
+
+demo1.makeProject({
+  title: "To-do app",
+  description: "A place to store all of your tasks",
+  tasks: [],
 });
-addTask(0, { title: "make tasks", description: "lorem..." });
-addTask(0, { title: "makee another" });
-addTask(0, { title: "make a project" });
-makeProject({ title: "second project", desctiption: "descriptione" });
-addTask(1, { title: "the only one" });
-printProjects();
+
+demo1.addTask(0, {
+  title: "Look around",
+  description: "Get to know the app.",
+});
+
+demo1.makeProject({
+  title: "Exercise solutions",
+  description: "Figure out an optimal setup for each major muscle group.",
+  tasks: [],
+});
+
+demo1.addTask(1, {
+  title: "Buy pulleys",
+  description: "For exercises that require an upward dirction of resistance.",
+});
+
+demo1.addTask(1, {
+  title: "Get weight plates",
+  description: "A minimalistic set that allows increments of 5lb.",
+});
+
+demo1.addTask(1, {
+  title: "Find supplies",
+  description: "Things like rope, webbing, caribiners, chains, foam... ",
+});
+
+demo1.addTask(1, {
+  title: "Make some equipment",
+  description: "Ask mom to sew together my inventions",
+});
+
+demo1.makeProject({
+  title: "250 box challenge",
+  description: '"...draw a box - then do it again another 249 times."',
+  tasks: [],
+});
+
+demo1.addTask(2, {
+  title: "Find drawing supplies",
+  description: "a large stack of priter paper and a .5 pen.",
+});
+
+demo1.addTask(2, {
+  title: "draw 5 boxes on a page",
+  description: "5 leaves enough room to check the convergences",
+});
+
+demo1.addTask(2, {
+  title: "50% rule",
+  description:
+    "Draw whatever you want for the same amount of time spent practicing.",
+});
+
+demo1.printProjects();
+
+const work = makeCategory("Work");
+
+work.makeProject({
+  title: "Block World",
+  description: "an original sandbox game where everything is blocky",
+});
--- src/program/to-do.js
+++ src/program/to-do.js
@@ -1,183 +1,228 @@
-export const projects = [];
+const categoryFactory = (title) => {
+  const setTitle = (input) => {
+    if (typeof input !== "string") {
+      return console.log(Error("The specified title is not a string."));
+    } else {
+      title = input;
+    }
+  };
 
-export const printProjects = (message = "A list of all Projects:") => {
-  if (projects.length === 0) {
-    return console.log(Error("There are no projects."));
-  }
-  console.log(`${message}`);
-  projects.forEach((project, index) => {
-    console.log(" ", index, JSON.parse(JSON.stringify(project)));
-    index++;
-  });
-};
+  const getTitle = () => {
+    return title;
+  };
 
-export const moveProject = (positionA, positionB) => {
-  if (!projects[positionA]) {
-    return console.log(Error("The specified origin index is out of range."));
-  }
-  if (!projects[positionB]) {
-    return console.log(
-      Error("The specified destination index is out of range.")
+  const projects = [];
+
+  const printProjects = (message) => {
+    if (projects.length === 0) {
+      return console.log(Error(`There are no projects in ${title} category.`));
+    }
+    if (!message) {
+      console.log(`A list of all projects in "${title}" category:`);
+    } else {
+      console.log(`${message} (${title} category)`);
+    }
+    projects.forEach((project, index) => {
+      console.log(" ", index, JSON.parse(JSON.stringify(project)));
+      index++;
+    });
+  };
+
+  const moveProject = (positionA, positionB) => {
+    if (!projects[positionA]) {
+      return console.log(Error("The specified origin index is out of range."));
+    }
+    if (!projects[positionB]) {
+      return console.log(
+        Error("The specified destination index is out of range.")
+      );
+    }
+    let objectA = projects.splice(positionA, 1)[0];
+    projects.splice(positionB, 0, objectA);
+    printProjects("The projects have been reordered.");
+  };
+
+  const removeProject = (index) => {
+    if (!projects[index]) {
+      return console.log(Error("The specified index is out of range."));
+    }
+    let title = projects[index].title;
+    projects.splice(index, 1);
+    printProjects(`Project "${title}" has been removed.`);
+  };
+
+  const makeProject = ({ title = "", description = "" }) => {
+    projects.push({ title, description, tasks: [] });
+    printProjects("A new project has been made.");
+  };
+
+  const taskFactory = ({
+    title = "",
+    description = "",
+    dueDate = "",
+    priority = "",
+  }) => {
+    let taskStatus = false;
+    const checkCompletionStatus = () => {
+      if (taskStatus) {
+        console.log(`Task "${title}" has been completed. :D`);
+      }
+      if (!taskStatus) {
+        console.log(`Task "${title}" has not been completed. :/`);
+      }
+      return taskStatus;
+    };
+    const changeCompletionStatus = () => {
+      taskStatus = taskStatus ? false : true;
+    };
+    return {
+      title,
+      description,
+      dueDate,
+      priority,
+      checkCompletionStatus,
+      changeCompletionStatus,
+    };
+  };
+
+  const printTasks = (projectIndex, message) => {
+    if (!projects[projectIndex]) {
+      return console.log(Error("The specified project index is out of range."));
+    }
+    if (!message) {
+      console.log(`Tasks in ${projects[projectIndex].title}.`);
+    } else {
+      console.log(`${message} (${title} category)`);
+    }
+    projects[projectIndex].tasks.forEach((task, index) => {
+      console.log(" ", index, task);
+      index++;
+    });
+  };
+
+  const addTask = (projectIndex, task = {}) => {
+    if (!projects[projectIndex]) {
+      return console.log(Error("The specified index is out of range."));
+    }
+    projects[projectIndex].tasks.push(taskFactory(task));
+    printTasks(
+      projectIndex,
+      `A task has been added to project "${projects[projectIndex].title}".`
     );
-  }
-  let objectA = projects.splice(positionA, 1)[0];
-  projects.splice(positionB, 0, objectA);
-  printProjects("The projects have been reordered.");
-};
+  };
 
-export const removeProject = (index) => {
-  if (!projects[index]) {
-    return console.log(Error("The specified index is out of range."));
-  }
-  let title = projects[index].title;
-  projects.splice(index, 1);
-  printProjects(`Project "${title}" has been removed.`);
-};
-
-export const makeProject = ({ title = "", description = "" }) => {
-  projects.push({ title, description, tasks: [] });
-  printProjects("A new project has been made.");
-};
-
-const taskFactory = ({
-  title = "",
-  description = "",
-  dueDate = "",
-  priority = "",
-}) => {
-  let taskStatus = false;
-  const checkCompletionStatus = () => {
-    if (taskStatus) {
-      console.log(`Task "${title}" has been completed. :D`);
+  const updateProject = (index, { title, description }) => {
+    if (!projects[index]) {
+      return console.log(Error("The specified project index is out of range."));
     }
-    if (!taskStatus) {
-      console.log(`Task "${title}" has not been completed. :/`);
+    if (title) {
+      projects[index].title = title;
     }
-    return taskStatus;
+    if (description) {
+      projects[index].description = description;
+    }
+    printProjects(`The project at ${index} has been updated.`);
   };
-  const changeCompletionStatus = () => {
-    taskStatus = taskStatus ? false : true;
-  };
-  return {
-    title,
-    description,
-    dueDate,
-    priority,
-    checkCompletionStatus,
-    changeCompletionStatus,
-  };
-};
 
-export const printTasks = (projectIndex, message) => {
-  if (!projects[projectIndex]) {
-    return console.log(Error("The specified project index is out of range."));
-  }
-  if (!message) {
-    console.log(`Tasks in ${projects[projectIndex].title}.`);
-  } else {
-    console.log(message);
-  }
-  projects[projectIndex].tasks.forEach((task, index) => {
-    console.log(" ", index, task);
-    index++;
-  });
-};
-
-export const addTask = (projectIndex, task = {}) => {
-  if (!projects[projectIndex]) {
-    return console.log(Error("The specified project index is out of range."));
-  }
-  projects[projectIndex].tasks.push(taskFactory(task));
-  printTasks(
+  const updateTask = (
     projectIndex,
-    `A task has been added to project "${projects[projectIndex].title}".`
-  );
+    taskIndex,
+    { title, description, dueDate, priority }
+  ) => {
+    if (!projects[projectIndex]) {
+      return console.log(Error("The specified project index is out of range."));
+    }
+    if (!projects[projectIndex].tasks[taskIndex]) {
+      return console.log(Error("The specified task index is out of range."));
+    }
+    let task = projects[projectIndex].tasks[taskIndex];
+    if (title) {
+      task.title = title;
+    }
+    if (description) {
+      task.description = description;
+    }
+    if (dueDate) {
+      task.dueDate = dueDate;
+    }
+    if (priority) {
+      task.priority = priority;
+    }
+    printTasks(projectIndex, `The task at ${taskIndex} has been updated.`);
+  };
+
+  const removeTask = (projectIndex, taskIndex) => {
+    if (!projects[projectIndex]) {
+      return console.log(Error("The specified project index is out of range."));
+    }
+    if (!projects[projectIndex].tasks[taskIndex]) {
+      return console.log(Error("The specified task index is out of range."));
+    }
+    let title = projects[projectIndex].tasks[taskIndex].title;
+    projects[projectIndex].tasks.splice(taskIndex, 1);
+    printTasks(projectIndex, `The task "${title}" has been removed.`);
+  };
+
+  const transferTask = (projectIndexA, projectIndexB, taskIndex) => {
+    if (!projects[projectIndexA]) {
+      return console.log(Error("The specified origin index is out of range."));
+    }
+    if (!projects[projectIndexB]) {
+      return console.log(
+        Error("The specified destination index is out of range.")
+      );
+    }
+    if (!projects[projectIndexA].tasks[taskIndex]) {
+      return console.log(Error("The specified task index is out of range."));
+    }
+    let task = projects[projectIndexA].tasks.splice(taskIndex, 1)[0];
+    projects[projectIndexB].tasks.push(taskFactory(task));
+    printTasks(projectIndexB, `Task ${task.title} has been transfered.`);
+  };
+
+  const moveTask = (projectIndex, positionA, positionB) => {
+    if (!projects[projectIndex]) {
+      return console.log(Error("The specified project index is out of range."));
+    }
+    if (!projects[projectIndex].tasks[positionA]) {
+      return console.log(
+        Error("The first specified task index is out of range.")
+      );
+    }
+    if (!projects[projectIndex].tasks[positionB]) {
+      return console.log(
+        Error("The second specified task index is out of range.")
+      );
+    }
+    let task = projects[projectIndex].tasks.splice(positionA, 1)[0];
+    projects[projectIndex].tasks.splice(positionB, 0, task);
+    printTasks(projectIndex, "The task list has been reordered");
+  };
+
+  return {
+    setTitle,
+    getTitle,
+    projects,
+    printProjects,
+    moveProject,
+    removeProject,
+    makeProject,
+    printTasks,
+    addTask,
+    updateProject,
+    updateTask,
+    removeTask,
+    transferTask,
+    moveTask,
+  };
 };
 
-export const updateProject = (index, { title, description }) => {
-  if (!projects[index]) {
-    return console.log(Error("The specified project index is out of range."));
-  }
-  if (title) {
-    projects[index].title = title;
-  }
-  if (description) {
-    projects[index].description = description;
-  }
-  printProjects(`The project at ${index} has been updated.`);
+const categories = [];
+
+const makeCategory = (title) => {
+  let project = categoryFactory(title);
+  categories.push(project);
+  return categories[categories.length - 1];
 };
 
-export const updateTask = (
-  projectIndex,
-  taskIndex,
-  { title, description, dueDate, priority }
-) => {
-  if (!projects[projectIndex]) {
-    return console.log(Error("The specified project index is out of range."));
-  }
-  if (!projects[projectIndex].tasks[taskIndex]) {
-    return console.log(Error("The specified task index is out of range."));
-  }
-  let task = projects[projectIndex].tasks[taskIndex];
-  if (title) {
-    task.title = title;
-  }
-  if (description) {
-    task.description = description;
-  }
-  if (dueDate) {
-    task.dueDate = dueDate;
-  }
-  if (priority) {
-    task.priority = priority;
-  }
-  printTasks(projectIndex, `The task at ${taskIndex} has been updated.`);
-};
-
-export const removeTask = (projectIndex, taskIndex) => {
-  if (!projects[projectIndex]) {
-    return console.log(Error("The specified project index is out of range."));
-  }
-  if (!projects[projectIndex].tasks[taskIndex]) {
-    return console.log(Error("The specified task index is out of range."));
-  }
-  let title = projects[projectIndex].tasks[taskIndex].title;
-  projects[projectIndex].tasks.splice(taskIndex, 1);
-  printTasks(projectIndex, `The task "${title}" has been removed.`);
-};
-
-export const transferTask = (projectIndexA, projectIndexB, taskIndex) => {
-  if (!projects[projectIndexA]) {
-    return console.log(Error("The specified origin index is out of range."));
-  }
-  if (!projects[projectIndexB]) {
-    return console.log(
-      Error("The specified destination index is out of range.")
-    );
-  }
-  if (!projects[projectIndexA].tasks[taskIndex]) {
-    return console.log(Error("The specified task index is out of range."));
-  }
-  let task = projects[projectIndexA].tasks.splice(taskIndex, 1)[0];
-  projects[projectIndexB].tasks.push(taskFactory(task));
-  printTasks(projectIndexB, `Task ${task.title} has been transfered.`);
-};
-
-export const moveTask = (projectIndex, positionA, positionB) => {
-  if (!projects[projectIndex]) {
-    return console.log(Error("The specified project index is out of range."));
-  }
-  if (!projects[projectIndex].tasks[positionA]) {
-    return console.log(
-      Error("The first specified task index is out of range.")
-    );
-  }
-  if (!projects[projectIndex].tasks[positionB]) {
-    return console.log(
-      Error("The second specified task index is out of range.")
-    );
-  }
-  let task = projects[projectIndex].tasks.splice(positionA, 1)[0];
-  projects[projectIndex].tasks.splice(positionB, 0, task);
-  printTasks(projectIndex, "The task list has been reordered");
-};
+export { makeCategory };
