1c3f01bd75a5209fa69e4ce436afa3687501c8f1
sau <dontsendsamspam@gmail.com>
2023-05-01 12:02:37 -0700

    Develop the application's appearance

--- src/program/main.js
+++ src/program/main.js
@@ -10,3 +10,15 @@ import {
 import { initiateSetup } from "./initiation-setup.js";
 
 initiateSetup();
+
+let leftPanel = document.getElementById("left-panel");
+let openLeftPanelBtn = document.getElementById("open-left-panel-btn");
+let closeLeftPanelBtn = document.getElementById("close-left-panel-btn");
+
+openLeftPanelBtn.addEventListener("click", () => {
+  leftPanel.classList.remove("-translate-x-full");
+});
+
+closeLeftPanelBtn.addEventListener("click", () => {
+  leftPanel.classList.add("-translate-x-full");
+});
